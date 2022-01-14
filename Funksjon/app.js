a3c311a8411712f63d02241c9c93e3b420cc1f2d
Sondre Mæhre <45791852+Sondrema@users.noreply.github.com>
2022-01-14 14:14:17 +0100

    Add files via upload

---
+++ Funksjon/app.js
@@ -0,0 +1,33 @@
+// Funksjoner
+
+function siHei (){
+  alert ("Hei");
+};
+
+
+
+// Calling the function
+
+siHei ();
+
+function heiNavn (value){
+  alert ("Hei " + value + "!")
+};
+
+
+
+function gangetall (tall1, tall2){
+  return tall1 * tall2;
+};
+
+var regnestykke = gangetall(4,5);
+
+alert (regnestykke);
+
+function deletall (tall3, tall4){
+  return tall3 / tall4;
+};
+
+var regnestykke1 = deletall (200,5);
+
+alert (regnestykke1);

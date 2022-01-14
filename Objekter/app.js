1651eb59458e382b6dc2d23dbfa5ef96b491a284
Sondre Mæhre <45791852+Sondrema@users.noreply.github.com>
2022-01-14 14:15:23 +0100

    Add files via upload

---
+++ Objekter/app.js
@@ -0,0 +1,28 @@
+// Objekter
+
+var user = {
+  firstName: "Sondre",
+  lastName: "Mæhre",
+  age: 30,
+  driverslicence: true
+}
+
+// alert(user.firstName)
+
+user.contry = "Norway";
+user.age = 31;
+
+//console.log(user);
+
+var questions = [
+  {
+    question: "Hva heter jeg?",
+    answer: "Sondre"
+  },
+  {
+    question: "Hvor gammel er jeg?",
+    answer: 27
+  }
+]
+
+alert(questions[1].question);

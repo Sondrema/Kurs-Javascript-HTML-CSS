351d80b50ba8c8bc5b7c3446b71af27c16804711
Sondre Mæhre <45791852+Sondrema@users.noreply.github.com>
2022-01-14 14:15:13 +0100

    Add files via upload

---
+++ Lister/app.js
@@ -0,0 +1,48 @@
+// Arrays / lister
+
+var taco = ["Kjøtt", "Lefser", "Ost", "Paprika", "Salat", "Rømme"];
+
+// var førsteIngrediens = taco [0];
+
+
+/*
+taco.length
+
+taco.indexOf("Mais")
+*/
+
+/*
+.push () - legger til på slutten av array
+.pop() - fjerner den siste verdien og returnerer verdien
+.unshift() - legger til på begynnelsen på en array.
+.shift() - fjerner den første verdien og returnerer den.
+*/
+/*
+console.log(taco);
+
+taco.push("Salsa");
+
+console.log(taco);
+
+taco.pop ();
+
+console.log(taco);
+
+taco.unshift ("Mais");
+
+console.log(taco);
+
+taco.shift(taco);
+
+console.log(taco);
+*/
+var kode = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+
+kode.[0][1];
+
+console.log(kode);
